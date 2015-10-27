bd1a3355d45e259f8330a46b882d7fbe99d7b311
Himer <xishizhaohua@qq.com>
2015-10-27 14:35:56 +0800

    Update singleflight.go

--- singleflight/singleflight.go
+++ singleflight/singleflight.go
@@ -31,8 +31,8 @@ type call struct {
 // Group represents a class of work and forms a namespace in which
 // units of work can be executed with duplicate suppression.
 type Group struct {
-	mu sync.Mutex       // protects m
-	m  map[string]*call // lazily initialized
+	mu sync.Mutex       // protects m/*保护m单线程访问*/
+	m  map[string]*call // lazily initialized/*一组同样的命令  一个string表示key 这个key代表的命名有同样的逻辑和返回*/
 }
 
 // Do executes and returns the results of the given function, making
@@ -46,19 +46,19 @@ func (g *Group) Do(key string, fn func() (interface{}, error)) (interface{}, err
 	}
 	if c, ok := g.m[key]; ok {
 		g.mu.Unlock()
-		c.wg.Wait()
-		return c.val, c.err
+		c.wg.Wait()/*所有的同key的命令全部等待第一个的执行完成*/
+		return c.val, c.err/*所有同key的命令返回第一个命令执行的结果*/
 	}
 	c := new(call)
 	c.wg.Add(1)
-	g.m[key] = c
+	g.m[key] = c/*第一个命令把自己加入到group的m中，并通过wg阻挡以后进来的同key命令*/
 	g.mu.Unlock()
 
 	c.val, c.err = fn()
-	c.wg.Done()
+	c.wg.Done()/*请他命令可以返回了，第一个命令还要把自己从group的m中删除*/
 
 	g.mu.Lock()
-	delete(g.m, key)
+	delete(g.m, key)/*这同key的命令全部执行返回了，从group的m中删除key*/
 	g.mu.Unlock()
 
 	return c.val, c.err
